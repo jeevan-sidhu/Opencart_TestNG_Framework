8c87475c23b325e81e148defe535c7ea55cda07b
Amanjeet <amanjeetcan23@gmail.com>
2024-10-06 15:01:05 -0400

    config prop file updated

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,6 +1,10 @@
 package testBase;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
@@ -18,11 +22,16 @@ public class BaseClass {
 	
 	public WebDriver driver;
 	public Logger logger;
+	public Properties p;
 	
 	@BeforeClass
 	@Parameters({"os","browser"})
-	public void setup(String os, String br)
+	public void setup(String os, String br) throws IOException
 	{
+		
+		FileReader file = new FileReader("./src//test//resources//config.properties");
+		p=new Properties();
+		p.load(file);
 		
 		logger =LogManager.getLogger(this.getClass());
 		
@@ -38,7 +47,8 @@ public class BaseClass {
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		
-		driver.get("https://tutorialsninja.com/demo/");
+		driver.get(p.getProperty("appURL"));
+		//driver.get("https://tutorialsninja.com/demo/");
 		driver.manage().window().maximize();
 	}
 	@AfterClass
