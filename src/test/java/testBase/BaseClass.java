cf9d46a72d8cdaad136e622791974012988d05bf
Amanjeet <amanjeetcan23@gmail.com>
2024-10-06 14:43:12 -0400

    parallel testing feature

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -6,9 +6,11 @@ import org.apache.commons.lang3.RandomStringUtils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-
+import org.testng.annotations.Parameters;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,12 +20,20 @@ public class BaseClass {
 	public Logger logger;
 	
 	@BeforeClass
-	public void setup()
+	@Parameters({"os","browser"})
+	public void setup(String os, String br)
 	{
 		
 		logger =LogManager.getLogger(this.getClass());
 		
-		driver = new ChromeDriver();
+		switch(br.toLowerCase())
+		{
+		case "chrome":  driver = new ChromeDriver(); break;
+		case "edge": driver =new EdgeDriver(); break;
+		case "firefox": driver =new FirefoxDriver(); break;
+		default: System.out.println("Invalid browser name..."); return;
+		}
+		
 		
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
