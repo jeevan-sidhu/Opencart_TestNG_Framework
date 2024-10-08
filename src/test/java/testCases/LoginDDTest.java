ba051b647280149a2517ce1d4e01eeba985ed685
Amanjeet <amanjeetcan23@gmail.com>
2024-10-08 11:13:01 -0400

    data driven feature

--- src/test/java/testCases/LoginDDTest.java
+++ src/test/java/testCases/LoginDDTest.java
@@ -1,5 +1,66 @@
 package testCases;
 
-public class LoginDDTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+import utilities.DataProviders;
+
+public class LoginDDTest extends BaseClass {
+
+	@Test(dataProvider="LoginData", dataProviderClass=DataProviders.class)
+	public void verify_loginDDT(String email, String pwd, String exp)
+	{
+		
+		logger.info("************Starting LoginDDT Test*************");
+		try
+		{
+		HomePage hp = new HomePage(driver);
+		hp.clickMyAccount();
+		hp.clickLogin();
+		
+		
+		LoginPage lp= new LoginPage(driver);
+		lp.setEmail(email);
+		lp.setPassword(pwd);
+		lp.clickLogin();
+		
+		
+		MyAccountPage macc = new MyAccountPage(driver);
+		boolean targetPage = macc.isMyAccountPageExist();
+		
+		if(exp.equalsIgnoreCase("Valid"))
+		{
+			if(targetPage==true)
+			{
+				macc.clickLogout();
+				Assert.assertTrue(true);
+			}
+			else
+			{
+				Assert.assertTrue(false);
+			}
+		}
+		if(exp.equalsIgnoreCase("Invalid"))
+		{
+			if(targetPage==true)
+			{
+				macc.clickLogout();
+				Assert.assertTrue(false);
+			}
+			else
+			{
+				Assert.assertTrue(true);
+			}
+		}
+		}
+		catch(Exception e)
+		{
+			Assert.fail();
+		}
+		logger.info("************Ending LoginDDT Test*************");
+	}
 }
